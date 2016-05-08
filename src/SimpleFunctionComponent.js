c8e74f7a9073cf5e15d66bb5ff279500ab4d0764
Nicole Rauch <nicole.m@gmx.de>
2016-05-08 16:12:13 +0200

    First component is up and running.

---
+++ src/SimpleFunctionComponent.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export default () => <p>Simple Function Component</p>
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import BabelPolyfill from "babel-polyfill";
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import SimpleFunctionComponent from "./SimpleFunctionComponent";
+
+ReactDOM.render(
+  <SimpleFunctionComponent />
+  , document.getElementById("root")
+);
+
