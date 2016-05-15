324b64382884f906f643e1d631c6222796681d80
Nicole Rauch <nicole.m@gmx.de>
2016-05-15 21:56:31 +0200

    Improved the name of the test.

--- test/LifecycleComponentTest.js
+++ test/LifecycleComponentTest.js
@@ -1,12 +1,10 @@
 import React from "react";
-import expect from "must";
 import { shallow } from "enzyme";
 
 import LifecycleComponent from "../src/LifecycleComponent";
 
 describe('LifecycleComponent', function () {
-
-  it('can observe state changes', function () {
+  it('executes most lifecycle methods', function () {
     console.log('Before rendering the component.');
     const wrapper = shallow(<LifecycleComponent />);
     console.log('After rendering the component.');
